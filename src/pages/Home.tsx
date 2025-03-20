b2054ff445cc6c81ca9af8890e0b041690ab0e20
서성진 <tjtjdwls0614@gmail.com>
2025-03-20 14:29:20 +0900

    feat: openweathermapAPI 활용 데이터 통신 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,17 +2,49 @@ import './Home.css';
 import { useState } from 'react';
 
 export default function Home() {
+  // 지역, 날짜 입력 상태 관리
   const [city, setCity] = useState('');
   const [date, setDate] = useState('');
+  // 날씨 데이터 상태 관리
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  // 지역 입력 핸들러
   const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
   };
 
+  // 날짜 입력 핸들러
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDate(e.target.value);
   };
 
+  // 날씨 데이터 가져오기
+  const getWeatherData = async (city: string) => {
+    const cityWithCountry = `${city},kr`;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityWithCountry}&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}&units=metric&lang=kr`);
+    return response.json();
+  };
+
+
+  // 추천 버튼 핸들러
+  const handleSubmit = async () => {
+    if (!city || !date) {
+      alert('지역과 날짜를 모두 입력해주세요');
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const data = await getWeatherData(city);
+      setWeatherData(data);
+      console.log('날씨 데이터:', data);
+    } catch (error) {
+      console.error('날씨 데이터 가져오기 실패:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
@@ -34,7 +66,10 @@ export default function Home() {
           value={date}
           onChange={handleDateChange}
         />
-        <button>추천</button>
+        <button
+          onClick={handleSubmit}>
+          {loading ? '로딩중...' : '추천'}
+        </button>
       </div>
     </div>
   )
