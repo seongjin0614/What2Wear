8877eb8e7a768fa7ed2d8f1e0d0c4d9ce65f8077
서성진 <tjtjdwls0614@gmail.com>
2025-03-20 14:06:02 +0900

    UI: 지역 및 날짜 입력

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
+import './Home.css';
+import { useState } from 'react';
+
 export default function Home() {
+  const [city, setCity] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+  };
+
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='homeTitle'>
+        <h1>What2Wear</h1>
+        <h3>지역, 날짜를 입력하면 날씨 정보를 바탕으로 옷을 추천해주는 서비스</h3>
+      </div>
+
+      <div className='infoInput'>
+        <input type="text" placeholder="지역" />
+        <input type="date" placeholder="날짜" />
+        <button>추천</button>
+      </div>
+    </div>
   )
 }
