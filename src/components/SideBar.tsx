b97f2176bd9e91006709641c3c5a03fd76b73a65
서성진 <tjtjdwls0614@gmail.com>
2025-04-09 00:08:16 +0900

    ui: sidebar 아이콘 추가 완료

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 //fontAwesome 아이콘
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faMagnifyingGlass, faCompass, faVideo, faPaperPlane, faHeart, faSquarePlus, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faMagnifyingGlass, faCompass, faVideo, faPaperPlane, faHeart, faSquarePlus, faUser, faBars, faBookOpenReader } from '@fortawesome/free-solid-svg-icons'
 
 const MenuItems = [
   { path: '/', label: '홈', icon: faHouse },
@@ -17,8 +17,6 @@ const MenuItems = [
   { path: '/profile', label: '프로필', icon: faUser },
   { path: '/wtw', label: '왓투웨어' },
   { path: '/mycloset', label: '나의옷장' },
-  { path: '/myrecord', label: '나의기록' },
-  { path: '/guestbook', label: "방명록" }
 ]
 
 
@@ -36,7 +34,8 @@ export default function SideBar() {
             <Link to={path}>{icon && <FontAwesomeIcon icon={icon} />} {label}</Link>
           </li>
         ))}
-        <li><Link to="/setting"><FontAwesomeIcon icon={faBars} /></Link> 더 보기</li>
+        <li><Link to="/gusetBook"><FontAwesomeIcon icon={faBookOpenReader} /> 방명록</Link></li>
+        <li><Link to="/setting"><FontAwesomeIcon icon={faBars} /> 더 보기</Link></li>
       </ul>
     </nav>
   )
