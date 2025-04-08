a68c1bf8dd1aef5f331defe187961ca7dea8ae20
서성진 <tjtjdwls0614@gmail.com>
2025-04-09 00:01:47 +0900

    ui: 탐색,릴스,메시지,알림,만들기,프로필,더보기 아이콘 추가

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 
 //fontAwesome 아이콘
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faMagnifyingGlass, faCompass, faVideo, faPaperPlane, faHeart, faSquarePlus, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
 
 const MenuItems = [
   { path: '/', label: '홈', icon: faHouse },
   { path: '/search', label: '검색', icon: faMagnifyingGlass },
+  { path: '/explore', label: '탐색', icon: faCompass },
+  { path: '/reels', label: '릴스', icon: faVideo },
+  { path: '/messages', label: '메시지', icon: faPaperPlane },
+  { path: '/notifications', label: '알림', icon: faHeart },
+  { path: '/create', label: '만들기', icon: faSquarePlus },
+  { path: '/profile', label: '프로필', icon: faUser },
   { path: '/wtw', label: '왓투웨어' },
   { path: '/mycloset', label: '나의옷장' },
   { path: '/myrecord', label: '나의기록' },
@@ -30,11 +36,7 @@ export default function SideBar() {
             <Link to={path}>{icon && <FontAwesomeIcon icon={icon} />} {label}</Link>
           </li>
         ))}
-      </ul>
-
-      <ul className='userMenu'>
-        <li>로그인</li>
-        <li>메시지</li>
+        <li><Link to="/setting"><FontAwesomeIcon icon={faBars} /></Link> 더 보기</li>
       </ul>
     </nav>
   )
