6a61c8faf5772f95f15553d84f5f658755d2ea68
서성진 <tjtjdwls0614@gmail.com>
2025-04-08 23:43:24 +0900

    ui: 홈 아이콘 추가

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,8 +2,12 @@ import './SideBar.css';
 
 import { Link } from 'react-router-dom';
 
+//fontAwesome 아이콘
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
 const MenuItems = [
-  { path: '/', label: '홈' },
+  { path: '/', label: '홈', icon: faHouse },
   { path: '/search', label: '검색' },
   { path: '/wtw', label: '왓투웨어' },
   { path: '/mycloset', label: '나의옷장' },
@@ -11,17 +15,19 @@ const MenuItems = [
   { path: '/guestbook', label: "방명록" }
 ]
 
+
+
 export default function SideBar() {
   return (
     <nav className="sideBar">
       <div className="sideBar-header">
-        <h1>서비스 이름!! 미정!</h1>
+        <h1>Outstagram</h1>
       </div>
 
       <ul className='Menu'>
-        {MenuItems.map(({ path, label }) => (
+        {MenuItems.map(({ path, label, icon }) => (
           <li key={path}>
-            <Link to={path}>{label}</Link>
+            <Link to={path}>{icon && <FontAwesomeIcon icon={icon} />} {label}</Link>
           </li>
         ))}
       </ul>
